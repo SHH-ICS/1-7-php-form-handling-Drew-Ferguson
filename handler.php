6aced5882868f16cd9397ba0b0c9baf9282ff3f5
Drew-Ferguson <drew.ferguson@stu.ocsb.ca>
2024-09-26 13:56:30 +0000

    Unit 1-07

--- handler.php
+++ handler.php
@@ -1,5 +1,23 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = htmlspecialchars($_POST['name']);
-  echo "Welcome, " . $name . "!";
-}
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Welcome Page</title>
+</head>
+
+<body>
+
+  <?php
+  $userName = "";
+  if (isset($_POST['userName'])) {
+    $userName = $_POST['userName'];
+  }
+  echo "<h1>Welcome " . $userName . "!</h1>\n";
+  ?>
+
+</body>
+
+</html>
